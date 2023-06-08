6297bb3cc303a70f46d959fb779e78a0d352f651
Bobur Yusupov <boburinfo2006@gmail.com>
2023-06-08 11:22:39 +0500

    Added Posts and Users components

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from "./posts/posts.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: '',
+      component: PostsComponent
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
   ],
   imports: [
     BrowserModule,
