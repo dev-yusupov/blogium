8c01b7a95699ab01725034d06df348ff47109325
Bobur Yusupov <bobur.yu@anur.tech>
2023-09-24 16:16:49 +0500

    Added new features

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import GitHub from "./GitHub";
 import { GiHamburgerMenu } from "react-icons/gi"
 
 const Navbar: React.FC = () => {
+
+    const NavbarRef = useRef<HTMLDivElement | any>(null);
+
+    const showNavbar = () => {
+        NavbarRef.current.classList.add("Navbar-Items-Active");
+        NavbarRef.current.classList.remove("Navbar-Items");
+    }
+    const hideNavbar = () => {
+        NavbarRef.current.classList.remove("Navbar-Items-Active");
+    }
 
     return (
         <div className="Navbar">
             <div className="Navbar-Logo">
                 <h1>Blogium</h1>
             </div>
-            <div className="Navbar-Items">
+            <div className="Navbar-Items" ref={NavbarRef}>
                 <ul>
                     <li className="Navbar-Item">
                         <a href="/">Home</a>
@@ -26,7 +36,7 @@ const Navbar: React.FC = () => {
                 <div className="Navbar-GitHub">
                     <GitHub />
                 </div>
-                <GiHamburgerMenu size={"27px"} color="#408085" />
+                <GiHamburgerMenu size={"27px"} color="#408085" className="Navbar-Hamburger" onClick={showNavbar} />
             </div>
         </div>
     )
